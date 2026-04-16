a928ca8b91468a2f89ace0c9d1b254222f1ce101
lv05327 <3251258336@qq.com>
2026-04-17 00:56:27 +0800

    initial structure

---
+++ entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/pages/Index.ts
@@ -0,0 +1,69 @@
+if (!("finalizeConstruction" in ViewPU.prototype)) {
+    Reflect.set(ViewPU.prototype, "finalizeConstruction", () => { });
+}
+interface Index_Params {
+    hasRedirected?: boolean;
+}
+import { AppInitializer } from "@bundle:com.example.hosn/entry/ets/app/AppInitializer";
+import { AppRouter } from "@bundle:com.example.hosn/entry/ets/app/AppRouter";
+class Index extends ViewPU {
+    constructor(parent, params, __localStorage, elmtId = -1, paramsLambda = undefined, extraInfo) {
+        super(parent, __localStorage, elmtId, extraInfo);
+        if (typeof paramsLambda === "function") {
+            this.paramsGenerator_ = paramsLambda;
+        }
+        this.hasRedirected = false;
+        this.setInitiallyProvidedValue(params);
+        this.finalizeConstruction();
+    }
+    setInitiallyProvidedValue(params: Index_Params) {
+        if (params.hasRedirected !== undefined) {
+            this.hasRedirected = params.hasRedirected;
+        }
+    }
+    updateStateVars(params: Index_Params) {
+    }
+    purgeVariableDependenciesOnElmtId(rmElmtId) {
+    }
+    aboutToBeDeleted() {
+        SubscriberManager.Get().delete(this.id__());
+        this.aboutToBeDeletedInternal();
+    }
+    private hasRedirected: boolean;
+    aboutToAppear(): void {
+        if (this.hasRedirected) {
+            return;
+        }
+        this.hasRedirected = true;
+        AppInitializer.initialize();
+        AppRouter.replace(AppRouter.getDefaultStartRoute());
+    }
+    initialRender() {
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Column.create();
+            Column.width('100%');
+            Column.height('100%');
+            Column.justifyContent(FlexAlign.Center);
+            Column.backgroundColor({ "id": 16777244, "type": 10001, params: [], "bundleName": "com.example.hosn", "moduleName": "entry" });
+            Column.padding({
+                left: { "id": 16777255, "type": 10002, params: [], "bundleName": "com.example.hosn", "moduleName": "entry" },
+                right: { "id": 16777255, "type": 10002, params: [], "bundleName": "com.example.hosn", "moduleName": "entry" }
+            });
+        }, Column);
+        this.observeComponentCreation2((elmtId, isInitialRender) => {
+            Text.create({ "id": 16777222, "type": 10003, params: [], "bundleName": "com.example.hosn", "moduleName": "entry" });
+            Text.fontSize({ "id": 16777257, "type": 10002, params: [], "bundleName": "com.example.hosn", "moduleName": "entry" });
+            Text.fontWeight(FontWeight.Medium);
+            Text.fontColor({ "id": 16777246, "type": 10001, params: [], "bundleName": "com.example.hosn", "moduleName": "entry" });
+        }, Text);
+        Text.pop();
+        Column.pop();
+    }
+    rerender() {
+        this.updateDirtyElements();
+    }
+    static getEntryName(): string {
+        return "Index";
+    }
+}
+registerNamedRoute(() => new Index(undefined, {}), "", { bundleName: "com.example.hosn", moduleName: "entry", pagePath: "pages/Index", pageFullPath: "entry/src/main/ets/pages/Index", integratedHsp: "false", moduleType: "followWithHap" });
---
+++ entry/src/main/ets/common/constants/RouteConstants.ts
@@ -0,0 +1,6 @@
+export class RouteConstants {
+  static readonly INDEX: string = 'pages/Index';
+  static readonly HOME: string = 'features/home/pages/HomePage';
+  static readonly NOTEBOOK_LIST: string = 'features/notebook/pages/NotebookListPage';
+  static readonly EDITOR: string = 'features/editor/pages/EditorPage';
+}
