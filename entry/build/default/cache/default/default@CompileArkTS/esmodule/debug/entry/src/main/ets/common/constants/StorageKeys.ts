3759d5fc64001b55adfde1553abf4ac64910e822
lv05327 <3251258336@qq.com>
2026-04-18 01:28:18 +0800

    add notebook list page, card and view model

--- entry/build/default/cache/default/default@CompileArkTS/esmodule/debug/entry/src/main/ets/common/constants/StorageKeys.ts
+++
@@ -1,6 +0,0 @@
-export class StorageKeys {
-    static readonly PREFERENCES_NAME: string = 'hosn_preferences';
-    static readonly NOTEBOOK_LIST: string = 'storage.notebook.list';
-    static readonly NOTEBOOK_SORT_TYPE: string = 'storage.notebook.sort_type';
-    static readonly LAST_OPENED_NOTEBOOK_ID: string = 'storage.notebook.last_opened_id';
-}
